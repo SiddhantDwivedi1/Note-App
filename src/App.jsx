627259972efda83395aacf5fe90ad8460866c556
SiddhantDwivedi1 <sid14.gkp@gmail.com>
2026-06-25 15:45:23 +0530

    UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,11 @@ const App = () => {
   }
 
   return (
-    <div className='h-screen bg-black text-white'>
+    <div className='h-screen lg:flex bg-black text-white'>
       <form onSubmit={(e) => {
         submitHandler(e)
-      }} className='flex justify-between gap-4 items-start flex-col p-10'>
+      }} className='flex gap-4 lg:w-1/2 items-start flex-col p-10'>
+        <h1 className='text-4xl font-bold'>Add Notes</h1>
 
         <input type="text" placeholder='Enter Notes Heading' className='px-5 py-2 w-full outline-none border-2 font-medium rounded' />
 
@@ -20,8 +21,12 @@ const App = () => {
 
         <button className='bg-white text-black px-5 py-2 w-full font-medium outline-none rounded'>Add Notes</button>
       </form>
-      <div>
-
+      <div className='lg:w-1/2 lg:border-l-2 p-10'>
+        <h1 className='text-4xl font-bold'>Recent Notes</h1>
+        <div className='flex flex-wrap gap-5 m-5 h-full overflow-auto'>
+          <div className='h-52 w-40 rounded-2xl bg-white'></div>
+          <div className='h-52 w-40 rounded-2xl bg-white'></div>
+        </div>
       </div>
     </div>
   )
