73330163a08e109eb4cf4744b75e076bde199b06
SiddhantDwivedi1 <sid14.gkp@gmail.com>
2026-06-27 12:43:26 +0530

    rename server.cjs to server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
